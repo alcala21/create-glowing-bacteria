ebd64604a0c020c20bf2592ed0f1058e1bb86e30
Carlos Alcala <alcala21@gmail.com>
2022-03-26 14:15:14 -0600

    Stage 2/6. Cut the plasmid.

--- bacteria.py
+++ bacteria.py
@@ -1,2 +1,5 @@
-complements = {"A": "T", "T": "A", "C": "G", "G": "C"}
-print("".join(complements[x] for x in input()))
+restrictases = ("CTGCAG", "GACGTC")
+cuts = {"CTGCAG": "C TGCAG", "GACGTC": "GACGT C"}
+dna_cuts = {x: y.replace(x, cuts[x]) for x, y in zip(restrictases, input().split())}
+for rest in restrictases:
+    print(dna_cuts[rest])
